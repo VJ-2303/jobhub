6e24ddda1188109e94757b231a277de3a51a2b4a
VJ-2303 <vanaraj1018@gmail.com>
2026-01-04 16:19:54 +0530

    Created project structure, and main package with required configurations to start an webserver

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	logger *log.Logger
+	config config
+}
+
+type config struct {
+	port        int
+	environment string
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "HTTP server port")
+	flag.StringVar(&cfg.environment, "environment", "DEV", "Environment (PROD | DEV | STAG )")
+
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		logger: logger,
+		config: cfg,
+	}
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: app.routes(),
+	}
+	logger.Printf("Starting %s server at %d", cfg.environment, cfg.port)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/VJ-2303/jobhub
+
+go 1.25.1
+
+require github.com/julienschmidt/httprouter v1.3.0
